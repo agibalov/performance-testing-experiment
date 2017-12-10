3b37165e3ac42b8d79cb4ce2a0a23bfc4de1e11a
loki2302 <loki2302@loki2302.me>
2017-12-10 14:46:40 -0500

    RetaskMeSimulation now has more requests

---
+++ gatling-experiment/src/gatling/scala/me/retask/RetaskMeSimulation.scala
@@ -0,0 +1,21 @@
+package me.retask
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class RetaskMeSimulation extends Simulation {
+  val conf = http.baseURL("http://retask.me")
+
+  val scn = scenario("Dummy scenario").repeat(100) {
+    exec(
+      exec(http("GET Home").get("/")),
+      exec(http("HEAD Home").get("/")),
+      exec(http("GET SignUp").get("/Account/SignUp")),
+      exec(http("GET SignIn").get("/Account/SignIn")),
+      exec(http("GET ForgotPassword").get("/Account/ForgotPassword")),
+      exec(http("GET Disclaimer").get("/Home/Disclaimer")))
+  }
+
+  setUp(scn.inject(atOnceUsers(10)))
+    .protocols(conf)
+}
