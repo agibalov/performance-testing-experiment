83c1d0726676b7a26acd74146ab7c4370cb48a56
Andrey Agibalov <906606+agibalov@users.noreply.github.com>
2021-02-13 16:53:58 -0500

    LABS-1461: switch to official gatling-gradle-plugin (#3)

--- gatling-experiment/src/gatling/scala/io/agibalov/DummySimulation.scala
+++ gatling-experiment/src/gatling/scala/io/agibalov/DummySimulation.scala
@@ -4,7 +4,7 @@ import io.agibalov.Predef._
 import io.gatling.core.Predef._
 
 class DummySimulation extends Simulation {
-  val dummyProtocol = dummy.something("something here")
+  val dummyProtocol = dummy().something("something here")
 
   val scn = scenario("Dummy scenario").repeat(10) {
     exec(dummy("omg").doSomething())
